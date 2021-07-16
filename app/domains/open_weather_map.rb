ef471d5384ef01658114f7fccd53142524baec82
Anja Tihic <atihic99@gmail.com>
2021-07-16 17:22:49 +0200

    Add city weather

---
+++ app/domains/open_weather_map.rb
@@ -0,0 +1,12 @@
+module OpenWeatherMap
+  class City
+    def self.city(city_name)
+      id = Resolver.city_id(city_name)
+      return nil if id.nil?
+
+      response = Faraday.get "https://api.openweathermap.org/data/2.5/weather?id=#{id}&appid=a32a4c61d2155b6c3327bc95b5ee7a2f"
+      hash_response = JSON.parse(response.body)
+      parse(hash_response)
+    end
+  end
+end
--- spec/domains/open_weather_map/city_spec.rb
+++ spec/domains/open_weather_map/city_spec.rb
@@ -57,4 +57,16 @@ RSpec.describe OpenWeatherMap::City do
       expect(city.temp_k).to eq(300.15)
     end
   end
+
+  describe '#city' do
+    it 'transforms JSON response into an OpenWeatherMap::City' do
+      city_object = described_class.city('Zagreb')
+      expect(city_object.lat).to eq(45.8144)
+    end
+
+    it 'returns nil if the city name is not recognised' do
+      city_object = described_class.city('nešto_bezveze')
+      expect(city_object).to be_nil
+    end
+  end
 end
