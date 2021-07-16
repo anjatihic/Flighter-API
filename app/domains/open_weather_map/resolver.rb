d369135ee1aaeed1703414683985ff568367a726
Anja Tihic <atihic99@gmail.com>
2021-07-16 14:13:31 +0200

    Change #city_id to class method

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -1,6 +1,6 @@
 module OpenWeatherMap
   class Resolver
-    def city_id(city_name)
+    def self.city_id(city_name)
       city_file = File.read(File.expand_path('city_ids.json', __dir__))
       city_hash = JSON.parse(city_file)
       found_city = city_hash.find { |city| city['name'] == city_name }
--- spec/domains/open_weather_map/resolver_spec.rb
+++ spec/domains/open_weather_map/resolver_spec.rb
@@ -1,11 +1,11 @@
 RSpec.describe OpenWeatherMap::Resolver do
   it 'when called with a known city name, the assert function returns the correct id' do
-    resolver_method = described_class.new.city_id('Ḩeşār-e Sefīd')
+    resolver_method = described_class.city_id('Ḩeşār-e Sefīd')
     expect(resolver_method).to eq(833)
   end
 
   it 'when called with an unknown city name, the assert function returns nil' do
-    resolver_method = described_class.new.city_id('žnj')
-    expect(resolver_method).to eq(nil)
+    resolver_method = described_class.city_id('žnj')
+    expect(resolver_method).to be_nil
   end
 end
