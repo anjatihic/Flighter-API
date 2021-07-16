04b469c451515b877d51b69a433fd46d00578b2c
Anja Tihic <atihic99@gmail.com>
2021-07-16 14:39:07 +0200

    Add parsing cities

--- app/domains/open_weather_map/city.rb
+++ app/domains/open_weather_map/city.rb
@@ -7,17 +7,27 @@ module OpenWeatherMap
       @id = id
       @lat = lat
       @lon = lon
-      @temp_k = temp_k
+      @temp_k = temp_k.round(2)
       @name = name
     end
 
     KELVIN_ZERO_CELSIUS = 273.15
     def temp
-      (temp_k - KELVIN_ZERO_CELSIUS).round(2)
+      (temp_k.round(2) - KELVIN_ZERO_CELSIUS).round(2)
     end
 
     def <=>(other)
       temp == other.temp ? name <=> other.name : temp <=> other.temp
+    end
+
+    def self.parse(params = {})
+      lat = params['coord']['lat']
+      lon = params['coord']['lon']
+      temp_k = params['main']['temp']
+      id = params['id']
+      name = params['name']
+
+      new(id: id, lat: lat, lon: lon, name: name, temp_k: temp_k)
     end
   end
 end
--- spec/domains/open_weather_map/city_spec.rb
+++ spec/domains/open_weather_map/city_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe OpenWeatherMap::City do
   # rubocop:disable Layout/LineLength
-  let(:city) { described_class.new(id: 2_172_797, lat: -16.92, lon: 145.77, name: 'Cairns', temp_k: 300) }
+  let(:city) { described_class.new(id: 2_172_797, lat: -16.92, lon: 145.77, name: 'Cairns', temp_k: 300.15) }
   let(:other_city) { described_class.new(id: 3_186_886, lat: 45.81, lon: 14.98, name: 'Zagreb', temp_k: 20) }
   # rubocop:enable Layout/LineLength
 
@@ -10,13 +10,13 @@ RSpec.describe OpenWeatherMap::City do
       expect(city.lat).to eq(-16.92)
       expect(city.lon).to eq(145.77)
       expect(city.name).to eq('Cairns')
-      expect(city.temp_k).to eq(300)
+      expect(city.temp_k).to eq(300.15)
     end
   end
 
   describe '#temp' do
     it 'converts temperature to celsius' do
-      expect(city.temp).to eq(26.85)
+      expect(city.temp).to eq(27.00)
     end
   end
 
@@ -29,7 +29,7 @@ RSpec.describe OpenWeatherMap::City do
     end
 
     it 'correctly compares two cities with same temperatures but different names' do
-      other_city.temp_k = 300
+      other_city.temp_k = 300.15
       expect(city < other_city).to eq(true)
 
       other_city.name = 'Bjelovar'
@@ -37,9 +37,24 @@ RSpec.describe OpenWeatherMap::City do
     end
 
     it 'correctly compares two cities with same temperatures and same names' do
-      other_city.temp_k = 300
+      other_city.temp_k = 300.15
       other_city.name = 'Cairns'
       expect(city < other_city).to eq(false)
+    end
+  end
+
+  describe '#parse' do
+    it 'correctly initialises an object from a hash' do
+      # rubocop:disable Layout/LineLength
+      hash = { 'coord' => { 'lat' => 145.77, 'lon' => -16.92 }, 'main' => { 'temp' => 300.15 }, 'id' => 2_172_797, 'name' => 'Cairns' }
+      # rubocop:enable Layout/LineLength
+      new_from_parse = described_class.parse(hash)
+
+      expect(new_from_parse.id).to eq(2_172_797)
+      expect(city.lat).to eq(-16.92)
+      expect(city.lon).to eq(145.77)
+      expect(city.name).to eq('Cairns')
+      expect(city.temp_k).to eq(300.15)
     end
   end
 end
