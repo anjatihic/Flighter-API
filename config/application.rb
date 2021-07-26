31209b40377f12dfea9a83f3f78f8cf102be8840
Anja Tihic <atihic99@gmail.com>
2021-07-26 13:37:51 +0200

    Revert to original configuration

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,6 @@
 require_relative "boot"
 
-require "rails"
-
-%w(
-  active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-).each do |railtie|
-  begin
-    require railtie
-  rescue LoadError
-  end
-end
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
