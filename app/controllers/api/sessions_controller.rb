d808476693877bfbe32915432c6a8bddebaf616f
Anja Tihic <atihic99@gmail.com>
2021-07-30 12:28:50 +0200

    Add status code to response

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,18 +1,21 @@
 module Api
   class SessionsController < ApplicationController
-    # POST /api/sessions
+    before_action :token_match, only: [:destroy]
+
+    # POST /api/session
     def create
       user = User.find_by(email: session_params[:email])
 
       if user.authenticate(session_params[:password])
         user.regenerate_token if user.token.nil?
 
-        render json: { session: { token: user.token, user: UserSerializer.render_as_hash(user) } }
+        render json: { session: { token: user.token, user: UserSerializer.render_as_hash(user) } }, status: :created # rubocop: disable Layout/LineLength
       else
         render json: { errors: { credentials: ['are invalid'] } }
       end
     end
 
+    # DELETE /api/session/:id
     def destroy
       user = User.find(params[:id])
       user.regenerate_token
